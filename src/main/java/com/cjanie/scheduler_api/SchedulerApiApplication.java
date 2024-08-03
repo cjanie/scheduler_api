77290f61df20356c008190d67dafd65668abc0ee
cjanie <janiechun@hotmail.fr>
2024-08-03 04:05:11 +0200

    utc test

--- src/main/java/com/cjanie/scheduler_api/SchedulerApiApplication.java
+++ src/main/java/com/cjanie/scheduler_api/SchedulerApiApplication.java
@@ -1,5 +1,9 @@
 package com.cjanie.scheduler_api;
 
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalField;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/test/java/com/cjanie/scheduler_api/UTCTests.java
@@ -0,0 +1,31 @@
+package com.cjanie.scheduler_api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+import org.junit.jupiter.api.Test;
+
+public class UTCTests {
+    
+    @Test
+    public void utc() {
+        // samedi à 03:00 à Tokyo, Japon correspond à
+        // vendredi à 18:00 dans le fuseau horaire Coordinated Universal Time (UTC)
+        LocalTime localTime = LocalTime.of(3, 0, 0);
+        LocalDate localDate = LocalDate.of(2024, 12, 23);
+        LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
+        ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.of("Asia/Tokyo"));
+        ZonedDateTime utcZonedDateTime = zonedDateTime.withZoneSameInstant(ZoneId.of("UTC"));
+        LocalTime utc = utcZonedDateTime.toLocalTime();
+        assertEquals(3, zonedDateTime.getHour());
+        assertEquals(18, utcZonedDateTime.getHour());
+        assertEquals(18, utc.getHour());
+    }
+}
